a8860995573188f5b41cf0e6cb1cf389ba6d0782
Forest <767425412lin@gmail.com>
2022-07-07 13:36:52 +0800

    feat: create user models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 
 @Module({
     imports: [UserModule],
-    controllers: [AppController, UserController],
+    controllers: [AppController],
     providers: [AppService],
 })
 export class AppModule {}
