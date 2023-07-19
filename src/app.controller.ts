3fad9db5b8b81ddee7348fcd71277a95e5c7f795
Forest <767425412lin@gmail.com>
2023-07-19 16:36:09 +0800

    切换到fastify

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Request, Response } from '@nestjs/common';
+import { FastifyReply, FastifyRequest } from 'fastify';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +7,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(
+    @Request() request: FastifyRequest,
+    @Response({ passthrough: true }) reply: FastifyReply,
+  ): string {
+    reply.header('url', request.url);
+    return 'hello';
   }
 }
