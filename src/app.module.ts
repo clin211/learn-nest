39e77bbe474baa7b9af1e6c3a7a31dc7167d30c9
Forest <767425412lin@gmail.com>
2023-07-19 10:59:51 +0800

    add dynamic module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BbbModule } from './bbb/bbb.module';
 
 @Module({
-  imports: [],
+  imports: [
+    BbbModule.register({
+      aaa: 1,
+      bbb: 2,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/bbb/bbb.module.ts
@@ -0,0 +1,18 @@
+import { DynamicModule, Module } from '@nestjs/common';
+import { BbbService } from './bbb.service';
+import { BbbController } from './bbb.controller';
+
+@Module({
+  controllers: [BbbController],
+  providers: [BbbService],
+})
+export class BbbModule {
+  static register(options: Record<string, any>): DynamicModule {
+    return {
+      module: BbbModule,
+      controllers: [BbbController],
+      providers: [{ provide: 'CONFIG_OPTIONS', useValue: options }, BbbService],
+      exports: [],
+    };
+  }
+}
